6c8ebe0513631c238ef31b12fa76776d551c3553
afaideen <pencouch>
2023-06-10 09:13:12 +0800

    Initial commit

---
+++ myapp/client.c/client.c.cpp
@@ -0,0 +1,20 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "..\..\wolfssl\options.h"
+
+#include "..\..\wolfssl/ssl.h"
+#include "..\..\wolfssl/test.h"
+
+#include <errno.h>
+#define SERV_PORT 11111
+
+int main(void)
+{
+	printf("Hi there!");
+	return 0;
+}
+
+
